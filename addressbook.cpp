7060e8e3f58d05424fbbe43f36c48d093434ca59
François <francois.drouhard@gmail.com>
2013-12-24 13:04:45 +0100

    boutons

--- addressbook.cpp
+++ addressbook.cpp
@@ -5,22 +5,29 @@
 #include <QGridLayout>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QString>
+#include <QMap>
 
 AddressBook::AddressBook(QWidget *parent)
     : QWidget(parent)
 {
     QLabel *labelNom        =   new QLabel (tr("Nom :"));
     lineEditNom             =   new QLineEdit;
+    lineEditNom->setReadOnly(true);
 
     QLabel *labelAdresse    =   new QLabel(tr("Adresse :"));
     textEditAdresse         =   new QTextEdit;
+    textEditAdresse->setReadOnly(true);
 
     QGridLayout *gridLayout = new QGridLayout;
     QVBoxLayout *layoutBoutons = new QVBoxLayout;
 
     boutonAjouter = new QPushButton (tr ("Ajouter"));
+    boutonAjouter->show();
     boutonSoumettre = new QPushButton (tr ("Soumettre"));
+    boutonSoumettre->hide();
     boutonAnnuler = new QPushButton (tr("Annuler"));
+    boutonAnnuler->hide();
 
     layoutBoutons->addWidget(boutonAjouter);
     layoutBoutons->addWidget(boutonSoumettre);
@@ -29,8 +36,8 @@ AddressBook::AddressBook(QWidget *parent)
     gridLayout->addWidget(labelNom , 0 , 0);
     gridLayout->addWidget(lineEditNom , 0 , 1);
     gridLayout->addWidget(labelAdresse , 1 , 0);
-    gridLayout->addWidget(textEditAdresse , 1 , 1);
-    gridLayout->addLayout(layoutBoutons , 1,2);
+    gridLayout->addWidget(textEditAdresse ,  1 , 1);
+    gridLayout->addLayout(layoutBoutons , 1 , 2);
 
     setLayout(gridLayout);
     setWindowTitle(tr("Carnet d'adresses"));
@@ -40,4 +47,17 @@ AddressBook::AddressBook(QWidget *parent)
 AddressBook::~AddressBook()
 {
     
+}
+
+/////////// Slots //////////
+void AddressBook::ajouterContact() {
+
+}
+
+void AddressBook::soumettreContact() {
+
+}
+
+void AddressBook::annuler() {
+
 }
--- addressbook.h
+++ addressbook.h
@@ -2,6 +2,7 @@
 #define ADDRESSBOOK_H
 
 #include <QWidget>
+#include <QMap>
 
 class QLabel;
 class QGridlayout;
@@ -9,6 +10,8 @@ class QLineEdit;
 class QTextEdit;
 class QPushButton;
 class QVBoxLayout;
+//class QMap;
+class QString;
 
 class AddressBook : public QWidget
 {
@@ -18,6 +21,11 @@ public:
     AddressBook(QWidget *parent = 0);
     ~AddressBook();
 
+public slots:
+    void ajouterContact ();
+    void soumettreContact ();
+    void annuler ();
+
 private:
     QLineEdit *lineEditNom;
     QTextEdit *textEditAdresse;
@@ -25,6 +33,10 @@ private:
     QPushButton *boutonAjouter;
     QPushButton *boutonSoumettre;
     QPushButton *boutonAnnuler;
+
+    QMap<QString , QString> listeContacts;
+    QString oldNom;
+    QString oldAdresse;
 };
 
 #endif // ADDRESSBOOK_H
